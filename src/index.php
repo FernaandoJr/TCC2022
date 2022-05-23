e576c0572484a0d31fe822dfc51dde827bb85abd
Fernando <kingprobryt@gmail.com>
2022-05-23 20:17:53 -0300

    criando paginas sobre/projetos/contato

--- src/index.php
+++ src/index.php
@@ -11,24 +11,21 @@
 <body>
     <div class="nav-container">
         <nav class="navbar">
-            <a class="navbar-logo" href="index.html"><img src="./img/logo_branco.png" class="logo-img"></a>
+            <a class="navbar-logo" href="/src/index.php"><img src="./img/logo_branco.png" class="logo-img"></a>
             <div class="menu-toggle" id="mobile-menu">
                 <span class="bar"></span>
                 <span class="bar"></span>
                 <span class="bar"></span>
             </div>
             <ul class="nav-menu">
-                <li><a href="#" class="nav-links nav-text">Início</a></li>
-                <li><a href="#" class="nav-links nav-text">Sobre</a></li>
-                <li><a href="#" class="nav-links nav-text">Projetos</a></li>
-                <li><a href="#" class="nav-links nav-text nav-links-btn">Contato</a></li>
+                <li><a href="./index.php" class="nav-links nav-text">Início</a></li>
+                <li><a href="./sobre.php" class="nav-links nav-text">Sobre</a></li>
+                <li><a href="./projetos.php" class="nav-links nav-text">Projetos</a></li>
+                <li><a href="./contato.php" class="nav-links nav-text nav-links-btn">Contato</a></li>
             </ul>
         </nav>
     </div>
     <script src="./js/navbar.js"></script>
-    <div class="blur-teste">
-        
-    </div>
     <div class="home-img-div">
         <div class="home-img-title-div">
             <h1 class="home-img-title-h1">Especializados em reformas e design de interiores residenciais</h1>
@@ -37,11 +34,10 @@
             <h1 class="home-img-subtitle-h1">Ambientes e decorações pensados para o bem-estar e conforto do cliente</h1>
         </div>
             <div class="home-btn-div">
-                <a class="home-btn-a" href="#">Fale Conosco</a>
+                <a class="home-btn-a" href="./contato.php">Fale Conosco</a>
             </div>
             <div class="home-img-gradient-effect"></div>
     </div>
-
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </body>
 </html>

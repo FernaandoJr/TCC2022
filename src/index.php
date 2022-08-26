c7880e93ccb44769a3b56de0da04ef678c3f8128
kingprobr <kingprobryt@gmail.com>
2022-08-26 14:32:07 -0300

    fazendo card sobre nos na home
    Co-Authored-By: Jesua Isaque <94057518+jesua07@users.noreply.github.com>

--- src/index.php
+++ src/index.php
@@ -43,16 +43,7 @@
                 <div class="home-btn-div">
                     <a href="contato.php" class="home-btn-a">Fale Conosco</a>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="blank5"></div>
-    <div class="home-card-sobre-title-div">
-        <p class="home-card-sobre-title-p">• Garantimos</p>
-    </div>
-    <div class="blank3"></div>
-
-    <!-- <div class="home-card-sobre-img-div">
+            </div><!-- <div class="home-card-sobre-img-div">
         <div class="home-card-sobre">
             <div class="home-card-sobre-img"></div>
             <div class="card-sobre-info">
@@ -67,9 +58,13 @@
             </div>
         </div>
     </div> -->
-
-
-
+        </div>
+    </div>
+    <div class="blank5"></div>
+    <div class="home-card-sobre-title-div">
+        <p class="home-card-sobre-title-p">• Garantimos</p>
+    </div>
+    <div class="blank3"></div>
     <div class="row1-container">
         <div class="card1">
             <div class="box red">
@@ -103,28 +98,41 @@
     </div>
     <div class="blank5"></div>
     <div class="blank3"></div>
+    <!-- CARD PROJETOS -->
     <div class="projeto-background-div">
         <h1>Conheça alguns dos nossos projetos</h1>
     </div>
     <div class="blank3 prj-bg"></div>
     <div class="parent2">
         <div class="parent">
-            <div class="div1"> <img src="./img/exemplo22.jpg" loading="lazy" alt=""></div>
-            <div class="div2"> <img src="./img/exemplo21.jpg" loading="lazy" alt=""></div>
-            <div class="div3"> <img src="./img/exemplo23.jpg" loading="lazy" alt=""></div>
-            <div class="div4"> <img src="./img/exemplo10.jpg" loading="lazy" alt=""></div>
-            <div class="div5"> <img src="./img/exemplo9.jpg" loading="lazy" alt=""></div>
-            <div class="div6"> <img src="./img/exemplo17.jpg" loading="lazy" alt=""></div>
-            <div class="div7"> <img src="./img/exemplo24.jpg" loading="lazy" alt=""></div>
-            <div class="div8"> <img src="./img/exemplo15.jpg" loading="lazy" alt=""></div>
-            <div class="div9"> <img src="./img/exemplo14.jpg" loading="lazy" alt=""></div>
-            <div class="div10"> <img src="./img/exemplo13.jpg" loading="lazy" alt=""></div>
+            <div class="div1"> <img src="./img/exemplo22.jpg" alt=""></div>
+            <div class="div2"> <img src="./img/exemplo21.jpg" alt=""></div>
+            <div class="div3"> <img src="./img/exemplo23.jpg" alt=""></div>
+            <div class="div4"> <img src="./img/exemplo10.jpg" alt=""></div>
+            <div class="div5"> <img src="./img/exemplo9.jpg" alt=""></div>
+            <div class="div6"> <img src="./img/exemplo17.jpg" alt=""></div>
+            <div class="div7"> <img src="./img/exemplo24.jpg" alt=""></div>
+            <div class="div8"> <img src="./img/exemplo15.jpg" alt=""></div>
+            <div class="div9"> <img src="./img/exemplo14.jpg" alt=""></div>
+            <div class="div10"> <img src="./img/exemplo13.jpg" alt=""></div>
         </div>
     </div>
-    <div class="blank3 prj-bg"></div>
-    <br><br><br><br><br><br><br><br>
+    <div class="blank4 prj-bg"></div>
+    <div class="projeto-background-div">
+        <h1>Saiba um pouco mais sobre nós</h1>
+    </div>
+    <div class="blank4 prj-bg"></div>
+<div class="sobre-card1 prj-bg">
+    <div class="sobre__card">
+        <div class="card-branco-dentro"></div>
+        <div class="card-sobre-img">
+            <img src="./img/exemplo21.jpg" alt="">
+        </div>
+    </div>
+</div>
+<div class="blank4 prj-bg"></div>
 
-
+<div class="blank15"></div>
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 </body>
 

2e0e942efe9372f1864194975adb2d8e332cfd83
kingprobr <kingprobryt@gmail.com>
2022-09-12 11:00:42 -0300

    tentando fazer login
    Co-Authored-By: Jesua Isaque <94057518+jesua07@users.noreply.github.com>

--- src/php/login.php
+++ src/php/login.php
@@ -2,19 +2,30 @@
 session_start();
 require_once 'init.php';
 
+
 $email = htmlspecialchars(isset($_POST['email']) ? $_POST['email'] : null);
 $senha = isset($_POST['senha']) ? $_POST['senha'] : null;
  
     if($email != null and $senha != null){
-        // Prepare uma declaração selecionada
         $PDO = db_connect();
-        $sql = "SELECT email, senha FROM cadastro WHERE email = :email and senha = :senha";
-   
-    } else{
-        if($senha == null){
-            echo "Por favor, insira sua senha.";
-        } else{
-            echo "Por favor, insira seu email.";
+        $sql = $PDO->query("SELECT email, senha FROM cadastro WHERE email = $email and senha = md5($senha)");
+        if($sql->rowCount() == 1){
+            $usuario = $sql->fetchAll(PDO::FETCH_ASSOC);
+            if (!isset($_SESSION)) {
+                session_start();
+            }
         }
-    }
-?>
+
+        $_SESSION['id'] = $usuario['id'];
+        $_SESSION['email'] = $usuario['email'];
+
+        header("Location: /src/php/painel.php");
+
+    } elseif($senha == null){
+            echo "Por favor, insira sua senha.";
+        } elseif($email == null){
+            echo "Por favor, insira seu email.";
+        }else{
+            echo "Email ou Senha incorretos.";
+        }
+
---
+++ src/php/painel.php
@@ -0,0 +1,5 @@
+<?php
+
+echo $_SESSION['id'];
+echo"<br>";
+echo $_SESSION['email'];

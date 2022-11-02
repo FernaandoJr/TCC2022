406adf8e327451720c6942b0a2404e05a261ae2d
Fernando <90939363+kingprobr@users.noreply.github.com>
2022-11-02 18:42:46 -0300

    colocando a tabela de pedidos na aba pedidos

--- src/admin/js/admin.js
+++ src/admin/js/admin.js
@@ -2,7 +2,12 @@ function AdminToggle(){
     let toggle = document.querySelector('.toggle');
     let navigation = document.querySelector('.navigation');
     let main = document.querySelector('.main');
+    let table = document.querySelector('.table');
     toggle.classList.toggle('active')
     navigation.classList.toggle('active')
     main.classList.toggle('active')
+}
+
+function MobileResize(){
+
 }
--- src/admin/pedidos.php
+++ src/admin/pedidos.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link rel="stylesheet" href="./styles/admin.css">
     <link rel="stylesheet" href="./styles/pedidos.css">
     <link rel="icon" href="/src/img/logo/logoA_blue.svg" type="image/svg">
     <title>Document</title>
 </head>
+
 <body>
     <div class="dash-container">
         <div class="navigation">
@@ -55,17 +57,74 @@
         <div class="topbar">
             <div class="toggle" onclick="AdminToggle();">
             </div>
-            <div class="search">
+            <div class="user">
+                <img src="\src\img\pfp2.jpeg" alt="">
+            </div>
+        </div>
+        <div class="pedidos-container">
+            <div class="title">
+                <p>Pedidos</p>
+
+                <div class="search">
                     <label>
                         <input type="text" placeholder="Pesquisar">
                         <i class="fa fa-solid fa-magnifying-glass"></i>
                     </label>
                 </div>
-            <div class="user">
-                <img src="\src\img\pfp2.jpeg" alt="">
             </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Tel</th>
+                        <th>E-mail</th>
+                        <th>Mensagem</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Rayssa Valentina Sônia Drumond</td>
+                        <td>8429389164</td>
+                        <td>rayssa-drumond83@pronta.com.br</td>
+                        <td>Quero fazer um planejamento de design de interior em uma cozinha...</td>
+                    </tr>
+                    <tr>
+                        <td>Nicole Sabrina Caroline Moura</td>
+                        <td>19982154881</td>
+                        <td>aparecida_pinto@marramoncks.com.br</td>
+                        <td>Quero reformar meu quarto e do meu filho...</td>
+                    </tr>
+                    <tr>
+                        <td>oio1i</td>
+                        <td>oio1i</td>
+                        <td>oio1i</td>
+                        <td>oioi1</td>
+                    </tr>
+                    <tr>
+                        <td>oioi</td>
+                        <td>oi1oi</td>
+                        <td>oio1i</td>
+                        <td>oioi1</td>
+                    </tr>
+                    <tr>
+                        <td>oio1i</td>
+                        <td>oio1i</td>
+                        <td>oio1i</td>
+                        <td>oi1oi</td>
+                    </tr>
+                    <tr>
+                        <td>oioi</td>
+                        <td>oioi</td>
+                        <td>oioi</td>
+                        <td>oioi</td>
+                    </tr>
+                </tbody>
+            </table>
+
         </div>
+
     </div>
     <script src="./js/admin.js"></script>
 </body>
+
 </html>
